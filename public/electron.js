14708a7960cd892766417e8b6c3a6be6a87ecb30
Douglas Camargo <camargo.douglas@icloud.com>
2020-05-25 23:38:32 -0300

    forgot to change the migrations dir

--- public/electron.js
+++ public/electron.js
@@ -18,7 +18,7 @@ const productionServer = new ProductionServer(3000); // botar no .env
 
 async function migrate() { 
   const knexFilePath = path.resolve(__dirname, '..', 'knexfile');
-  const migrationsFilePath = path.resolve(__dirname, '..', 'src', 'migrations'); 
+  const migrationsFilePath = path.resolve(__dirname, '..', 'database', 'migrations'); 
   const pending = await knexMigrate('pending', { knexfile: knexFilePath, migrations: migrationsFilePath }, () => {});
 
   if ( pending > 0 ) {
